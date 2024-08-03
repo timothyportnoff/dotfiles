d216d60ab95f1809daed109affcf27691ed1bab2
timothyportnoff <timothyportnoff@gmail.com>
2024-08-03 00:37:48 +0000

    Re-Built setup top handle a list of dotfiles and folders. Easier to add files and folders.

--- setup.sh
+++ setup.sh
@@ -1,23 +1,37 @@
 #!/bin/bash
 
-# List of dotfiles
-DOTFILES=".bash_aliases .bashrc .vimrc .gitconfig .hushlogin"
+# List of dotfiles. Follow the pattern to add files/folders.
+declare -A DOTFILES
+DOTFILES=(
+    [".bash_aliases"]="$HOME/"  					#Aliases
+    [".bashrc"]="$HOME/"  						#Bash File
+    [".vimrc"]="$HOME/" 							#Vim
+    [".gitconfig"]="$HOME/" 						#Git
+    [".hushlogin"]="$HOME/.hushlogin" 						#Silence Term MOTD
+    ["config.conf"]="$HOME/.config/neofetch/" 	#You gotta have a cool terminal
+    [".vim"]="$HOME/" 	#You gotta have a cool terminal
+)
 
-for file in $DOTFILES; do
-    if [ -f ~/$file ]; then #Checks for existing file
-        echo "Backing up existing $file to $file.backup"
-        mv ~/$file ~/$file.backup
+# Main Script - Create Symbolic Links
+for file in "${!DOTFILES[@]}"; do
+    target_dir="${DOTFILES[$file]}"
+    target_file="$target_dir/$(basename $file)"
+
+    # Create target directory if it doesn't exist
+    mkdir -p $target_dir
+
+    # Check for existing file and backup if necessary
+    if [ -f $target_file ]; then
+        echo "Backing up existing $(basename $file) to $(basename $file).backup"
+        mv $target_file $target_file.backup
     fi
-    echo "Creating symlink for $file"
-    ln -s ~/dotfiles/$file ~/$file
-done
 
+    # Create the symlink
+    echo "Creating symlink for $file in $target_dir"
+    ln -s -f ~/dotfiles/$file $target_file
+done
 echo "All dotfiles have been symlinked."
 
-#Link .vim folder for colorschemes, code complete, and more
-ln -s -i ~/dotfiles/.vim/ ~/
-echo "Vim folder has been symlinked."
-
-
+#Exit 
 echo "IT IS RECOMMENDED THAT YOU CHECK YOUR BACKUP FILES AND DELETE AS NECESSARY."
-echo ""
+echo "Have a great day :)"
